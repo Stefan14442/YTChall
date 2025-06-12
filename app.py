1e53b63d81f861cc3b9facc7d1dc7a2d88a3a69f
Stefan14442 <stefanps1235@gmail.com>
2025-06-12 15:39:50 +0200

    Updated YouTube earnings calculator for AdSense

--- app.py
+++ app.py
@@ -110,5 +110,13 @@ def index():
 
     return render_template("index.html", error=error, result=result)
 
+@app.route("/privacy")
+def privacy():
+    return render_template("privacy.html")
+
+@app.route("/terms")
+def terms():
+    return render_template("terms.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
